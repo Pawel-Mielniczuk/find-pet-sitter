99ce18c35df68fcbcf84b3d4814a076db0455ea3
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-03 15:52:45 +0100

    build: install and setup react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import React from "react";
 
 import { AuthProvider } from "../context/AuthContext";
 
 export default function RootLayout() {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 60 * 24,
+            retry: 1,
+          },
+        },
+      }),
+  );
   return (
-    <AuthProvider>
-      <Slot />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Slot />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
