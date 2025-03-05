3a8feb0a1f76b496cea364504eb1c691cd6879f7
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-05 10:21:42 +0100

    build: setup and install react native testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
   globals: {
     __dirname: true,
   },
-  extends: ["expo", "prettier", "plugin:import/recommended"],
-  plugins: ["prettier", "simple-import-sort"],
+  extends: ["expo", "prettier", "plugin:import/recommended", "plugin:testing-library/react"],
+  plugins: ["prettier", "simple-import-sort", "testing-library"],
   rules: {
     "prettier/prettier": "error",
     "no-console": "error",
@@ -12,5 +12,16 @@ module.exports = {
     "simple-import-sort/imports": "error",
     "import/named": "off",
     "react/jsx-no-duplicate-props": "off",
+    "testing-library/await-async-queries": "error",
+    "testing-library/no-await-sync-queries": "error",
+    "testing-library/no-debugging-utils": "warn",
+    "testing-library/no-manual-cleanup": "error",
+    "testing-library/prefer-screen-queries": "error",
   },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
 };
