ccc92fdef8547c1a508d72021cc6f4d3bca42df7
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-05 12:08:42 +0100

    feat: create const variable with error messages

---
+++ src/formValidations/errorMessages.ts
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGES = {
+  REQUIRED: "This field is required",
+  INVALID_EMAIL: "Email is invalid",
+  PASSWORD_TOO_SHORT: "Password must be at least 6 characters",
+  PASSWORD_MISMATCH: "Passwords do not match",
+  ROLE_REQUIRED: "Please select your role",
+  CONFIRM_PASSWORD: "Please confirm your password",
+};
