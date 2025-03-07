2c1f0cde2260feb7e924dce72c227fe3a3173f81
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-07 18:46:09 +0100

    refactor: replace state variable with const variable

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -15,18 +15,6 @@ export const queryClient = new QueryClient({
 });
 
 export default function RootLayout() {
-  // const [queryClient] = React.useState(
-  //   () =>
-  //     new QueryClient({
-  //       defaultOptions: {
-  //         queries: {
-  //           staleTime: 1000 * 60 * 5,
-  //           gcTime: 1000 * 60 * 60 * 24,
-  //           retry: 1,
-  //         },
-  //       },
-  //     }),
-  // );
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
