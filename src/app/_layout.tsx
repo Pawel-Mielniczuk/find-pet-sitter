613010cf1c77ded3c4a4150f75e618e8d5233d6f
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-07 18:39:00 +0100

    refactor: refactred queryClient: replaced useState with const variable

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,19 +4,29 @@ import React from "react";
 
 import { AuthProvider } from "../context/AuthContext";
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 60 * 24,
+      retry: 1,
+    },
+  },
+});
+
 export default function RootLayout() {
-  const [queryClient] = React.useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60 * 5,
-            gcTime: 1000 * 60 * 60 * 24,
-            retry: 1,
-          },
-        },
-      }),
-  );
+  // const [queryClient] = React.useState(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           staleTime: 1000 * 60 * 5,
+  //           gcTime: 1000 * 60 * 60 * 24,
+  //           retry: 1,
+  //         },
+  //       },
+  //     }),
+  // );
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
