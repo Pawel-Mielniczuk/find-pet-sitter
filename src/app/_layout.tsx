0f565efe150d4bbfd961cfb49f60fda22c74f438
Pawel Mielniczuk <mrjeronimo@onet.pl>
2025-03-18 20:35:05 +0100

    feat: create and use NotificationContext

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,35 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Slot } from "expo-router";
+import * as Notifications from "expo-notifications";
+import { Slot, SplashScreen } from "expo-router";
 import React from "react";
 
 import { AuthProvider } from "../context/AuthContext";
+import { NotificationProvider } from "../context/NotificationsContext";
 import { PetsProvider } from "../context/PetsContext";
 import { SearchProvider } from "../context/SearchContext";
 import { queryClient } from "../lib/queryClient";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <PetsProvider>
-          <SearchProvider>
-            <Slot />
-          </SearchProvider>
-        </PetsProvider>
+        <NotificationProvider>
+          <PetsProvider>
+            <SearchProvider>
+              <Slot />
+            </SearchProvider>
+          </PetsProvider>
+        </NotificationProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
